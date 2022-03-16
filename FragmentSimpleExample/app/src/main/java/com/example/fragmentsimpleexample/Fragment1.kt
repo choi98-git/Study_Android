01105a4316213a84071fc61708fb580dbb6d20cf
최건영 <choy2736@naver.com>
2022-03-16 16:31:30 +0900

    Fragment simple example with viewbinding

---
+++ FragmentSimpleExample/app/src/main/java/com/example/fragmentsimpleexample/Fragment1.kt
@@ -0,0 +1,18 @@
+package com.example.fragmentsimpleexample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import java.util.zip.Inflater
+
+class Fragment1 : Fragment(R.layout.fragment1) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment1, container, false)
+    }
+}
---
+++ FragmentSimpleExample/app/src/main/java/com/example/fragmentsimpleexample/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.fragmentsimpleexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.PagerAdapter
+import com.example.fragmentsimpleexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fragment1Btn.setOnClickListener {
+
+            replaceFragment(Fragment1())
+
+        }
+
+        binding.fragment2Btn.setOnClickListener {
+
+            replaceFragment(Fragment2())
+
+
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer,fragment)
+        fragmentTransaction.commit()
+    }
+}
